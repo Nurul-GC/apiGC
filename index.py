c600e1b34de90152a2c57780f0e311c29a5b16ed
Nurul-GC <Nurul-GC@github.com>
2021-03-30 17:46:59 +0200

    updating commit

--- index.py
+++ index.py
@@ -1,3 +1,13 @@
+"""
+script principal do projecto
+para executa-lo basta navegar ate a localização atual do script
+e digitar no terminal:
+*** linux ***
+    ~$: python3 ./index.py
+*** windows ***
+    C:/localizacao/do/script/> py index.py
+"""
+
 # ************************************************
 #  (c) 2019-2021 Nurul-GC                        *
 # ************************************************
